1fba97d59fed9c3038e61096b2c7797239949e18
yasingultekin <yasingultekin13@gmail.com>
2022-07-07 14:41:13 +0300

    first commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,5 @@
-import reactLogo from '../../assets/react.svg';
-import './Header.css';
-
 const Header = () => {
-  return (
-    <div className="logo-container">
-      <img className="react-logo" src={reactLogo} alt="react-logo" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Header;
